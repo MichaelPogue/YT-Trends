f25cd334773cc203ae2ba1d59a013e776037b20e
Michael Pogue <115908053+MichaelPogue@users.noreply.github.com>
2023-02-24 19:11:44 -0600

    Replaced consumer text with producer.

--- scrape.py
+++ scrape.py
@@ -24,7 +24,7 @@ EMAIL = os.getenv('EMAIL')
 host = 'localhost'
 rmq_queue = "01-smoker"
 rmq_deque = deque(maxlen = 5)
-rmq_limit = 15
+rmq_limit = 0 #######################
 
 
 response = requests.get(URL).text
@@ -82,113 +82,130 @@ Step 2: Data Producer
 -------------------------------------------------------------------------------
 """
 
-# define a callback function to be called when a message is received
-def decode_message(ch, method, properties, body):
-    """ 
-    Function to decode individual lines of data and monitor for temperature fluctuations.
-    ---------------------------------------------------------------------------
-    """
-    # decode the binary message body to a string
-    print(f" [x] Received {body.decode()}")
+""" Clear Queue
+-----------------------------------------------------------------------------"""
+def queue_delete(host: str, queue: str):
+    # Connect to RabbitMQ.
+    conn = pika.BlockingConnection(pika.ConnectionParameters(host))
+    # Set channel.
+    ch = conn.channel()
+    # Clear the queues from the given variable.
+    ch.queue_delete(queue)
 
-    # Set initial queue data used to create deque.
-    data_deque.append(body.decode())
-    initial_data = data_deque[0]
-    initial_split = initial_data.split(",")
-    initial_temp = float(initial_split[1][:-1])
-    # initial_temp = str(re.findall(r"[-+]?\d*\.\d+", deque_1_initial)) <- A monument to failure.
+""" Message Processing
+-----------------------------------------------------------------------------"""
+def read_csv():
+    # Declared Variables.
+    host = "localhost"
+    queue_1 = "01-smoker"
+    queue_2 = "02-food-A"
+    queue_3 = "03-food-B"
 
-    # Add continuous data to array.
-    present_data = body.decode()
-    present_split = present_data.split(",")
-    present_temp = float(present_split[1][:-1])
-    # present_temp = str(re.findall(r"[-+]?\d*\.\d+", deque_1_present)) <- A monument to failure.
+    # CSV File Setup.
+    # Set csv file to be used.
+    csv_file = open("smoker-temps.csv", "r")
+    # Create reader with a comma delimiter.
+    reader = csv.reader(csv_file, delimiter=",")
+    # Skip header in the dataset.
+    next(reader)
 
-    # Calculate temperature difference from the initial input with the latest.
-    temperature_difference = abs(float(initial_temp) - float(present_temp))
+    # Clear Queue
+    # Clear individual queues to ensure queue is empty.
+    queue_delete(host, queue_1)
+    queue_delete(host, queue_2)
+    queue_delete(host, queue_3)
 
-    # Calculate if the temperature is within parameters, then send a warning message.
-    if temperature_difference >= data_warning:
-        degree = "\u00b0"+"F"
-        print(f"ERROR: Temperature fluctuation detected.")
-        print(f"       A change of {round(temperature_difference, 1)}{degree} change within 2.5 minutes.")
+    # Read Line by Line in a CSV Document
+    for row in reader:
+        # Set "id" as the times which all smokers have in common.
+        smoker_time_id = row[0]
 
-    ch.basic_ack(delivery_tag = method.delivery_tag)
-    time.sleep(5)
+        # 01-smoker Queue Reader.
+        try:
+            # Set the next row in line to column 1.
+            c1_row_text = float(row[1])
+            # Configure row text to be sent as a message.
+            row_text = f"[{smoker_time_id}, {c1_row_text}]"
+            # Encode message for sending.
+            message = row_text.encode()
+            # Send data to RabbitMQ via the send_message function.
+            send_message(host, queue_1, message)
+            # Error dump. 
+        except ValueError:
+            pass
 
-# define a main function to run the program
-def main(hn: str, qn: str):
-    """ 
-    Continuously listen for task messages on a named queue.
-    ---------------------------------------------------------------------------
-    """
-    # when a statement can go wrong, use a try-except block
+        # 02-food-A Queue Reader.
+        try:
+            # Set the next row in line to column 2.
+            c2_row_text = float(row[2])
+            # Configure row text to be sent as a message.
+            row_text = f"[{smoker_time_id}, {c2_row_text}]"
+            # Encode message for sending.
+            message = row_text.encode()
+            # Send data to RabbitMQ via the send_message function.
+            send_message(host, queue_2, message)
+            # Error dump. 
+        except ValueError:
+            pass
+
+        # 03-food-B Queue Reader.
+        try:
+            # Set the next row in line to column 3.
+            c3_row_text = float(row[3])
+            # Configure row text to be sent as a message.
+            row_text = f"[{smoker_time_id}, {c3_row_text}]"
+            # Encode message for sending.
+            message = row_text.encode()
+            # Send data to RabbitMQ via the send_message function.
+            send_message(host, queue_3, message)
+            # Error dump. 
+        except ValueError:
+            pass
+
+""" Send Message to RabbitMQ Servers
+-----------------------------------------------------------------------------"""
+def send_message(host: str, queue_name: str, message):
     try:
-        # try this code, if it works, keep going
         # create a blocking connection to the RabbitMQ server
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host = hn))
-
-    # except, if there's an error, do this
-    except Exception as e:
-        print()
-        print("ERROR: connection to RabbitMQ server failed.")
-        print(f"Verify the server is running on host={hn}.")
-        print(f"The error says: {e}")
-        print()
-        sys.exit(1)
-
-    try:
+        conn = pika.BlockingConnection(pika.ConnectionParameters(host))
         # use the connection to create a communication channel
-        channel = connection.channel()
-
+        ch = conn.channel()
         # use the channel to declare a durable queue
         # a durable queue will survive a RabbitMQ server restart
         # and help ensure messages are processed in order
         # messages will not be deleted until the consumer acknowledges
-        channel.queue_declare(queue = qn, durable = True)
-
-        # The QoS level controls the # of messages
-        # that can be in-flight (unacknowledged by the consumer)
-        # at any given time.
-        # Set the prefetch count to one to limit the number of messages
-        # being consumed and processed concurrently.
-        # This helps prevent a worker from becoming overwhelmed
-        # and improve the overall system performance. 
-        # prefetch_count = Per consumer limit of unaknowledged messages      
-        channel.basic_qos(prefetch_count=1) 
-
-        # configure the channel to listen on a specific queue,  
-        # use the callback function named callback,
-        # and do not auto-acknowledge the message (let the callback handle it)
-        channel.basic_consume( queue = qn, on_message_callback = decode_message)
-
+        ch.queue_declare(queue=queue_name, durable=True)
+        # use the channel to publish a message to the queue
+        # every message passes through an exchange
+        ch.basic_publish(exchange="", routing_key=queue_name, body=message)
         # print a message to the console for the user
-        print(" [*] Ready for work. To exit press CTRL+C")
-
-        # start consuming messages via the communication channel
-        channel.start_consuming()
-
-    # except, in the event of an error OR user stops the process, do this
-    except Exception as e:
-        print()
-        print("ERROR: something went wrong.")
-        print(f"The error says: {e}")
+        print(f" [x] Sent {message} from {queue_name}")
+        time.sleep(.1)
+    except pika.exceptions.AMQPConnectionError as e:
+        print(f"ERROR! Connection to RabbitMQ server failed: {e}")
         sys.exit(1)
-    except KeyboardInterrupt:
-        print()
-        print(" User interrupted continuous listening process.")
-        sys.exit(0)
     finally:
-        print("\nClosing connection. Goodbye.\n")
-        connection.close()
+        # close the connection to the server
+        conn.close()
+
+""" Code Command Center
+-----------------------------------------------------------------------------"""
+def main():
+    # # Ask to launch website for monitoring. 
+    # a = input("Open RabbitMQ monitoring website? (y or n)")
+    # if a == "y":
+    #     webbrowser.open_new("http://localhost:15672/#/queues")
+    # else:
+    #     pass
+    # Begin task of sending message.
+    read_csv()
 
 """  
-
+Launch Code!
 ------------------------------------------------------------------------------------------ """
 # Standard Python idiom to indicate main program entry point
 # This allows us to import this module and use its functions
 # without executing the code below.
 # If this is the program being run, then execute the code below
 if __name__ == "__main__":
-    # call the main function with the information needed
-    main(host, data_queue)
+    main()
