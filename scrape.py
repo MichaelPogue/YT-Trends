ecf773b44c74416a70e4fdbedb282b361af61fc7
Michael Pogue <115908053+MichaelPogue@users.noreply.github.com>
2023-02-25 12:08:08 -0600

    Created class and methods to organize script.

--- scrape.py
+++ scrape.py
@@ -16,9 +16,6 @@ from time import strftime
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 
-""" 
-----------------------------------------------------------------------------"""
-# Set API parameters. 
 load_dotenv()
 # URL = os.getenv('YOUTUBE_LINK')
 URL = 'https://www.youtube.com/gaming/games'
@@ -27,116 +24,127 @@ FILE_NAME = 'streamstatistics'
 
 host = 'localhost'
 queue = 'yt_streamstatistics'
-ytdeque = deque(maxlen = 5)
-limit = 0 #######################
 
 response = requests.get(URL).text
 soup = BeautifulSoup(response, 'html.parser')
 primary_data = soup.body.find_all('script')[13].contents[0]
 
-data_name = []
-data_views = []
 
-""" 
-----------------------------------------------------------------------------"""
-try: 
-    game_data = (
-            json.loads(primary_data[20:-1])
-            ['contents']
-            ['twoColumnBrowseResultsRenderer']
-            ['tabs'][0]
-            ['tabRenderer']
-            ['content']
-            ['sectionListRenderer']
-            ['contents'][0]
-            ['itemSectionRenderer']
-            ['contents'][0]
-            ['shelfRenderer']
-            ['content']
-            ['gridRenderer']
-            ['items']
-        )
+class ytData:
+    """ 
+    ----------------------------------------------------------------------------"""
+    def __init__(self, primary_data) -> None:
+        self.primary_data = primary_data
+    
+    def collect_data(self):
+        data_name = []
+        data_views = []
 
-    for game in game_data:
-        details = (
-            game
-            ['gameCardRenderer']
-            ['game']
-            ['gameDetailsRenderer']
-        )
+        """ 
+        ----------------------------------------------------------------------------"""
+        try: 
+            game_data = (
+                    json.loads(primary_data[20:-1])
+                    ['contents']
+                    ['twoColumnBrowseResultsRenderer']
+                    ['tabs'][0]
+                    ['tabRenderer']
+                    ['content']
+                    ['sectionListRenderer']
+                    ['contents'][0]
+                    ['itemSectionRenderer']
+                    ['contents'][0]
+                    ['shelfRenderer']
+                    ['content']
+                    ['gridRenderer']
+                    ['items']
+                )
 
-        game_data_name = details['title']['simpleText']
-        game_data_views = details['liveViewersText']['runs'][0]['text']
+            for game in game_data:
+                details = (
+                    game
+                    ['gameCardRenderer']
+                    ['game']
+                    ['gameDetailsRenderer']
+                )
 
-        data_name.append(game_data_name)
-        data_views.append(game_data_views)
-except Exception:
-    pass
+                game_data_name = details['title']['simpleText']
+                game_data_views = details['liveViewersText']['runs'][0]['text']
 
-name = pd.DataFrame(data_name, columns = [
-    'name'])
-views = pd.DataFrame(data_views, columns = [
-    'views'])
-
-all_data = name.join(views)
-
-all_data.to_csv(f'{FILE_NAME}.csv')
-
-""" Step 2: Data Producer
-----------------------------------------------------------------------------"""
-def rabbitmq_admin_site_offer():
-    """Offer to open the RabbitMQ Admin website"""
-    answer = input("Would you like to monitor RabbitMQ queues? y or n ")
-    if answer.lower() == "y":
-        webbrowser.open_new("http://localhost:15672/#/queues")
-        print()
-
-""" 
-----------------------------------------------------------------------------"""
-def queue_delete(host: str, queue: str):
-    conn = pika.BlockingConnection(pika.ConnectionParameters(host))
-    ch = conn.channel()
-    ch.queue_delete(queue)
-
-""" 
-----------------------------------------------------------------------------"""
-def read_csv():
-    csv_file = open(f'{FILE_NAME}.csv', 'r', encoding = 'utf-8')
-    reader = csv.reader(csv_file, delimiter=",")
-    next(reader)
-
-    for row in reader:
-        game_name = row[1]
-        game_views = row[2]
-        try:
-            row_text = f"[{game_name}, {game_views}]"
-            message = row_text.encode()
-            send_message(host, queue, message)
-        except ValueError:
+                data_name.append(game_data_name)
+                data_views.append(game_data_views)
+        except Exception:
             pass
 
-""" 
-----------------------------------------------------------------------------"""
-def send_message(host: str, queue: str, message):
-    try:
+        name = pd.DataFrame(data_name, columns = [
+            'name'])
+        views = pd.DataFrame(data_views, columns = [
+            'views'])
+
+        all_data = name.join(views)
+        all_data.to_csv(f'{FILE_NAME}.csv')
+
+        # return all_data
+
+class ytSend:
+    """ Step 2: Data Producer
+    ----------------------------------------------------------------------------"""
+    def rabbitmq_admin_site_offer():
+        """Offer to open the RabbitMQ Admin website"""
+        answer = input("Would you like to monitor RabbitMQ queues? y or n ")
+        if answer.lower() == "y":
+            webbrowser.open_new("http://localhost:15672/#/queues")
+            print()
+
+    """ 
+    ----------------------------------------------------------------------------"""
+    def queue_delete(host: str, queue: str):
         conn = pika.BlockingConnection(pika.ConnectionParameters(host))
         ch = conn.channel()
-        ch.queue_declare(queue = queue, durable = True)
-        ch.basic_publish(exchange = '', routing_key = queue, body = message)
-        print(f"Message Sent: {message}")
-        time.sleep(1)
-    except pika.exceptions.AMQPConnectionError as e:
-        print(f"ERROR! Connection to RabbitMQ server failed: {e}")
-        sys.exit(1)
-    finally:
-        conn.close()
+        ch.queue_delete(queue)
+
+    """ 
+    ----------------------------------------------------------------------------"""
+    def read_csv():
+        csv_file = open(f'{FILE_NAME}.csv', 'r', encoding = 'utf-8')
+        reader = csv.reader(csv_file, delimiter=",")
+        next(reader)
+
+        for row in reader:
+            game_name = row[1]
+            game_views = row[2]
+            try:
+                row_text = f"[{game_name}, {game_views}]"
+                message = row_text.encode()
+                ytSend.send_message(host, queue, message)
+            except ValueError:
+                pass
+
+    """ 
+    ----------------------------------------------------------------------------"""
+    def send_message(host: str, queue: str, message):
+        try:
+            conn = pika.BlockingConnection(pika.ConnectionParameters(host))
+            ch = conn.channel()
+            ch.queue_declare(queue = queue, durable = True)
+            ch.basic_publish(exchange = '', routing_key = queue, body = message)
+            print(f"Message Sent: {message}")
+            time.sleep(1)
+        except pika.exceptions.AMQPConnectionError as e:
+            print(f"ERROR! Connection to RabbitMQ server failed: {e}")
+            sys.exit(1)
+        finally:
+            conn.close()
 
 """ 
 ----------------------------------------------------------------------------"""
 def main():
-    queue_delete(host, queue)
+    yts = ytSend
+    ytd = ytData
+    ytd.collect_data(primary_data)
+    yts.queue_delete(host, queue)
     # rabbitmq_admin_site_offer()
-    read_csv()
+    yts.read_csv()
 
 """ 
 ----------------------------------------------------------------------------"""
