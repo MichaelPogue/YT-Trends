3307ecbdd72994117da6fe11d0c7154b5f310813
Michael Pogue <115908053+MichaelPogue@users.noreply.github.com>
2023-02-25 18:20:16 -0600

    Update yt_consumer.py
    Completed consumer queue

--- yt_consumer.py
+++ yt_consumer.py
@@ -1,8 +1,3 @@
-""" 
-Authored By: Michael Pogue | Created on: 22Feb23 | Last Updated: 25Feb23 
-
-"""
-
 import pika
 import sys
 import time
@@ -11,52 +6,74 @@ from collections import deque
 from time import strftime 
 from dotenv import load_dotenv
 
-load_dotenv()
-EMAIL = os.getenv("EMAIL_ADDRESS")
-FILE_NAME = 'streamstatistics'
-
 host = 'localhost'
 queue = 'yt_streamstatistics'
 data_deque = deque(maxlen = 5)
-data_warning = 100000
+data_warning = 16000
 
-def callback(ch, method, properties, body):
-    print(f"Received: {body.decode()} at {strftime('%H:%M:%S')}")
-    time.sleep(body.count(b"."))
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+load_dotenv()
+EMAIL = os.getenv("EMAIL_ADDRESS")
+
+def decode_message(ch, method, properties, body):
+    """  """
+    print(f"Received:      {body.decode()} at {strftime('%H:%M:%S')}")
+    data_deque.append(body.decode())
+    initial_data = body.decode()
+    initial_split = initial_data.split(",")
+    data = initial_split[1][:-1]
+
+    if character_detection(data, '.') == True:
+        remove_period = data.replace('.', '')
+        remove_letter = remove_period.replace('K','')
+        data_final = int(remove_letter)*100
+        # print(f'--------------> {data_final}')
+    elif character_detection(data, '.') == False:
+        remove_letter = data.replace('K','')
+        data_final = int(remove_letter)*1000
+        # print(f'--------------> {data_final}')
+
+    if data_final >= data_warning:
+        print("Received:      ")
+        print("--------------> BIG NUMBER")
+    elif data_final < data_warning:
+        pass
+
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+    time.sleep(1)
+
+def character_detection(string, chars):
+    for char in string:
+        if char in chars:
+            return True
+    return False
 
 def main(hn: str, qn: str):
+    """  """
     try:
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host=hn))
-
-
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host = hn))
     except Exception as e:
-        print("\nERROR: connection to RabbitMQ server failed.")
-        print(f"Verify the server is running on host = {hn}.")
-        print(f"Error Message: {e}.")
+        print("ERROR: connection to RabbitMQ server failed.")
+        print(f"Verify the server is running on host={hn}.")
+        print(f"The error says: {e}")
         sys.exit(1)
-
     try:
         channel = connection.channel()
-        channel.queue_declare(queue=qn, durable=True)
+        channel.queue_declare(queue = qn, durable = True)
         channel.basic_qos(prefetch_count=1) 
-        channel.basic_consume( queue=qn, on_message_callback=callback)
-        print("Listening, press CTRL+C to exit.")
+        channel.basic_consume( queue = qn, on_message_callback = decode_message)
+        print("Ready for work. To exit press CTRL+C.")
         channel.start_consuming()
-
-
     except Exception as e:
-        print("\nERROR: Something went wrong.")
+        print("ERROR: Something went wrong.")
         print(f"The error says: {e}")
         sys.exit(1)
     except KeyboardInterrupt:
         print("\nUser interrupted continuous listening process.")
         sys.exit(0)
     finally:
-        print("\nConnection terminated.")
+        print("\nClosing connection. Goodbye.\n")
         connection.close()
 
+"""  """
 if __name__ == "__main__":
-    host = 'localhost'
-    queue = "yt_streamstatistics"
     main(host, queue)
