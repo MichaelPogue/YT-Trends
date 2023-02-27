caa5761d4df20187cd6bc9bc50112c28c86930ec
Michael Pogue <115908053+MichaelPogue@users.noreply.github.com>
2023-02-26 20:42:17 -0600

    Added email support

--- yt_consumer.py
+++ yt_consumer.py
@@ -2,25 +2,31 @@ import pika
 import sys
 import time
 import os
+import ssl
+import smtplib
 from collections import deque
 from time import strftime 
 from dotenv import load_dotenv
+from email.message import EmailMessage
+
+load_dotenv()
+EMAIL_SENDER = os.getenv('EMAIL_SENDER')
+EMAIL_SENDER_PASSWORD = os.getenv('EMAIL_SENDER_PASSWORD')
+EMAIL_RECEIVER = os.getenv('EMAIL_RECEIVER')
 
 host = 'localhost'
 queue = 'yt_streamstatistics'
-data_deque = deque(maxlen = 5)
-data_warning = 16000
-
-load_dotenv()
-EMAIL = os.getenv("EMAIL_ADDRESS")
+data_deque = deque(maxlen = 1)
+data_warning = 15000
 
 def decode_message(ch, method, properties, body):
     """  """
-    print(f"Received:      {body.decode()} at {strftime('%H:%M:%S')}")
+    print(f"Received: {body.decode()} at {strftime('%H:%M:%S')}")
     data_deque.append(body.decode())
     initial_data = body.decode()
     initial_split = initial_data.split(",")
     data = initial_split[1][:-1]
+    name = initial_split[0]
 
     if character_detection(data, '.') == True:
         remove_period = data.replace('.', '')
@@ -33,13 +39,35 @@ def decode_message(ch, method, properties, body):
         # print(f'--------------> {data_final}')
 
     if data_final >= data_warning:
-        print("Received:      ")
-        print("--------------> BIG NUMBER")
+        send_message(name, data_final)
     elif data_final < data_warning:
         pass
 
     ch.basic_ack(delivery_tag = method.delivery_tag)
     time.sleep(1)
+
+def send_message(name, data_final):
+    subject = f"Project YT-TREND Alert: {name} at {data_final}"
+    body = f"""
+    Automated Alert Message: 
+
+    The game {name} has {data_final} views. 
+
+    This automated alert message was triggered due to a view score higher than: {data_warning}.
+    """
+
+    em = EmailMessage()
+
+    em['From'] = EMAIL_SENDER
+    em['To'] = EMAIL_RECEIVER
+    em['Subject'] = subject
+    em.set_content(body)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com',465, context = context) as smtp:
+        smtp.login(EMAIL_SENDER, EMAIL_SENDER_PASSWORD)
+        smtp.sendmail(EMAIL_SENDER, EMAIL_RECEIVER, em.as_string())
 
 def character_detection(string, chars):
     for char in string:
