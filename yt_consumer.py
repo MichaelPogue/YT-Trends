2f8e9aba17313c65e8306a8ef315434372be7fe7
Michael Pogue <115908053+MichaelPogue@users.noreply.github.com>
2023-02-26 20:47:04 -0600

    Placed code within class.

--- yt_consumer.py
+++ yt_consumer.py
@@ -19,89 +19,95 @@ queue = 'yt_streamstatistics'
 data_deque = deque(maxlen = 1)
 data_warning = 15000
 
-def decode_message(ch, method, properties, body):
-    """  """
-    print(f"Received: {body.decode()} at {strftime('%H:%M:%S')}")
-    data_deque.append(body.decode())
-    initial_data = body.decode()
-    initial_split = initial_data.split(",")
-    data = initial_split[1][:-1]
-    name = initial_split[0]
+class ytReceive:
+    def decode_message(ch, method, properties, body):
+        """  """
+        print(f"Received: {body.decode()} at {strftime('%H:%M:%S')}")
+        data_deque.append(body.decode())
+        initial_data = body.decode()
+        initial_split = initial_data.split(",")
+        data = initial_split[1][:-1]
+        name = initial_split[0]
 
-    if character_detection(data, '.') == True:
-        remove_period = data.replace('.', '')
-        remove_letter = remove_period.replace('K','')
-        data_final = int(remove_letter)*100
-        # print(f'--------------> {data_final}')
-    elif character_detection(data, '.') == False:
-        remove_letter = data.replace('K','')
-        data_final = int(remove_letter)*1000
-        # print(f'--------------> {data_final}')
+        if ytReceive.character_detection(data, '.') == True:
+            remove_period = data.replace('.', '')
+            remove_letter = remove_period.replace('K','')
+            data_final = int(remove_letter)*100
+            # print(f'--------------> {data_final}')
+        elif ytReceive.character_detection(data, '.') == False:
+            remove_letter = data.replace('K','')
+            data_final = int(remove_letter)*1000
+            # print(f'--------------> {data_final}')
 
-    if data_final >= data_warning:
-        send_message(name, data_final)
-    elif data_final < data_warning:
-        pass
+        if data_final >= data_warning:
+            ytReceive.send_message(name, data_final)
+        elif data_final < data_warning:
+            pass
 
-    ch.basic_ack(delivery_tag = method.delivery_tag)
-    time.sleep(1)
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+        time.sleep(1)
 
-def send_message(name, data_final):
-    subject = f"Project YT-TREND Alert: {name} at {data_final}"
-    body = f"""
-    Automated Alert Message: 
+    def send_message(name, data_final):
+        subject = f"Project YT-TREND Alert: {name} at {data_final}"
+        body = f"""
+        Automated Alert Message: 
 
-    The game {name} has {data_final} views. 
+        The game {name} has {data_final} views. 
 
-    This automated alert message was triggered due to a view score higher than: {data_warning}.
-    """
+        This automated alert message was triggered due to a view score higher than: {data_warning}.
+        """
 
-    em = EmailMessage()
+        em = EmailMessage()
 
-    em['From'] = EMAIL_SENDER
-    em['To'] = EMAIL_RECEIVER
-    em['Subject'] = subject
-    em.set_content(body)
+        em['From'] = EMAIL_SENDER
+        em['To'] = EMAIL_RECEIVER
+        em['Subject'] = subject
+        em.set_content(body)
 
-    context = ssl.create_default_context()
+        context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL('smtp.gmail.com',465, context = context) as smtp:
-        smtp.login(EMAIL_SENDER, EMAIL_SENDER_PASSWORD)
-        smtp.sendmail(EMAIL_SENDER, EMAIL_RECEIVER, em.as_string())
+        with smtplib.SMTP_SSL('smtp.gmail.com',465, context = context) as smtp:
+            smtp.login(EMAIL_SENDER, EMAIL_SENDER_PASSWORD)
+            smtp.sendmail(EMAIL_SENDER, EMAIL_RECEIVER, em.as_string())
 
-def character_detection(string, chars):
-    for char in string:
-        if char in chars:
-            return True
-    return False
+    def character_detection(string, chars):
+        for char in string:
+            if char in chars:
+                return True
+        return False
 
-def main(hn: str, qn: str):
-    """  """
-    try:
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host = hn))
-    except Exception as e:
-        print("ERROR: connection to RabbitMQ server failed.")
-        print(f"Verify the server is running on host={hn}.")
-        print(f"The error says: {e}")
-        sys.exit(1)
-    try:
-        channel = connection.channel()
-        channel.queue_declare(queue = qn, durable = True)
-        channel.basic_qos(prefetch_count=1) 
-        channel.basic_consume( queue = qn, on_message_callback = decode_message)
-        print("Ready for work. To exit press CTRL+C.")
-        channel.start_consuming()
-    except Exception as e:
-        print("ERROR: Something went wrong.")
-        print(f"The error says: {e}")
-        sys.exit(1)
-    except KeyboardInterrupt:
-        print("\nUser interrupted continuous listening process.")
-        sys.exit(0)
-    finally:
-        print("\nClosing connection. Goodbye.\n")
-        connection.close()
+    def receive_message(hn: str, qn: str):
+        """  """
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host = hn))
+        except Exception as e:
+            print("ERROR: connection to RabbitMQ server failed.")
+            print(f"Verify the server is running on host={hn}.")
+            print(f"The error says: {e}")
+            sys.exit(1)
+        try:
+            channel = connection.channel()
+            channel.queue_declare(queue = qn, durable = True)
+            channel.basic_qos(prefetch_count=1) 
+            channel.basic_consume( queue = qn, on_message_callback = ytReceive.decode_message)
+            print("Ready for work. To exit press CTRL+C.")
+            channel.start_consuming()
+        except Exception as e:
+            print("ERROR: Something went wrong.")
+            print(f"The error says: {e}")
+            sys.exit(1)
+        except KeyboardInterrupt:
+            print("\nUser interrupted continuous listening process.")
+            sys.exit(0)
+        finally:
+            print("\nClosing connection. Goodbye.\n")
+            connection.close()
+
+def main():
+    ytr = ytReceive
+    ytr.receive_message(host, queue)
+
 
 """  """
 if __name__ == "__main__":
-    main(host, queue)
+    main()
